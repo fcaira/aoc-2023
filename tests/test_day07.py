d58844448d17fdbc198d46b34979e1d794558c39
freddie <freddie.caira@protonmail.com>
2023-12-08 10:32:12 +0000

    Day 7 part 1 complete

--- tests/test_day07.py
+++ tests/test_day07.py
@@ -2,7 +2,11 @@ import pytest
 
 from typing import List, Union
 
-from solutions.day07 import part1, part2
+from solutions.day07 import (
+    part1,
+    part2,
+    add_ordering,
+)
 from tests.conftest import get_input
 
 day = "07"
@@ -10,11 +14,21 @@ eg_input = get_input(f"solutions/example_inputs/{day}")
 input = get_input(f"solutions/inputs/{day}")
 
 
+def test_add_ordering():
+    assert add_ordering(eg_input) == [
+        ("32T3K", "765", [2, 2, 1, 9, 2, 12]),
+        ("T55J5", "684", [4, 9, 4, 4, 10, 4]),
+        ("KK677", "28", [3, 12, 12, 5, 6, 6]),
+        ("KTJJT", "220", [3, 12, 9, 10, 10, 9]),
+        ("QQQJA", "483", [4, 11, 11, 11, 10, 13]),
+    ]
+
+
 @pytest.mark.parametrize(
     argnames="input, expected_output",
     argvalues=[
         (eg_input, 6440),
-        (input, 0),
+        (input, 246912307),
     ],
     ids=["eg", "ans"],
 )
