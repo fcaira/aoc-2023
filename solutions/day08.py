94a481a692c1eea00682ea4ed57a31c14e26fafe
freddie <freddie.caira@protonmail.com>
2023-12-15 17:52:24 +0000

    Remove unused memo, bit embarrassing

--- solutions/day08.py
+++ solutions/day08.py
@@ -1,29 +1,23 @@
+from collections import deque
 from re import findall
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
 
 
 def parse_map(raw_map: Tuple[str, Dict[str, Dict[str, str]]]):
     directions = raw_map[0]
     map = {}
     for ln in raw_map[2:]:
-        nodes = findall(r"[A-Z]{3}", ln)
+        nodes = findall(r"[A-Z0-9]{3}", ln)
         map.update({nodes[0]: {"L": nodes[1], "R": nodes[2]}})
     return directions, map
 
 
-def part1(i):
+def part1(i: List[str]) -> int:
     directions, map = parse_map(i)
-    memo = {}
     node = "AAA"
     steps = 0
-    turns_taken = ''
     while node != "ZZZ":
         dir = directions[steps % len(directions)]
-        turns_taken += dir
-        if turns_taken in memo:
-            node = memo[turns_taken]
-        else:
-            node = map[node][dir]
-            memo.update({turns_taken: node})
+        node = map[node][dir]
         steps += 1
     return steps
